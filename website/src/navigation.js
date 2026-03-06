fea9270cfe10bd07629deae9f0c5d06ae3231143
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2026-03-06 20:50:24 +1000

    remvoed blog page

--- website/src/navigation.js
+++ website/src/navigation.js
@@ -1,4 +1,4 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
@@ -17,10 +17,6 @@ export const headerData = {
     {
       text: 'Services',
       href: getPermalink('/services'),
-    },
-    {
-      text: 'Blog',
-      href: getPermalink('/blog'),
     },
   ],
   actions: [{ text: 'Contact', href: '/contact', target: '_blank', icon: 'tabler:mail'}],
@@ -43,12 +39,6 @@ export const footerData = {
       ],
     },
     {
-      title: 'Blog',
-      links: [
-        { text: 'View blog', href: '/blog' },
-      ],
-    },
-    {
       title: 'Contact Us',
       links: [
         { text: 'contact@fovrinteractive.net', href: 'mailto:contact@fovrinteractive.net' },
@@ -65,7 +55,6 @@ export const footerData = {
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/fovr-interactive/' },
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/FovrInteractive' },
     { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/@fovrinteractive' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
     &copy; 2025 FoVR Interactive. All rights reserved.
